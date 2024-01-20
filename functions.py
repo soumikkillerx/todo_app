07d5d29c05540326ad7e96c1e3fd6e1bf6d59800
soumikkillerx <soumikparida12345@gmail.com>
2024-01-20 10:58:58 +0530

    commited function change

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
-filename='files/todos.txt'
-def get_todos(filename='files/todos.txt'):
-    file = open(filename, 'r')
+filename = 'files/todos.txt'
+
+
+def get_todos(filename_todo=filename):
+    file = open(filename_todo, 'r')
     todos = file.readlines()
     file.close()
     return todos
 
 
-def write_todos(todos,filename='files/todos.txt'):
+def write_todos(todos,filename_todo=filename):
     file = open(filename, 'w')
     file.writelines(todos)
     file.close()
